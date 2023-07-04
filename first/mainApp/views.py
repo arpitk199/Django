b162da9e47c1fda5f6ff6719edd351f59d7a6c14
arpitk199 <kumararpi199@gmail.com>
2023-07-04 18:33:57 +0530

    commit

--- first/mainApp/views.py
+++ first/mainApp/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 def homePage(Request):
-    return render(Request, "index.html" )
+    lv = 0
+    lc = 0
+    uv = 0
+    uc = 0
+    d = 0
+    s = 0
+    sp = 0
+    show = False
+    if(Request.method=="POST"):
+        show = True
+        message = Request.POST.get("message")
+        for i in message:
+            if(i>='a' and i<='z'):
+                if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+                    lv = lv + 1
+                else:
+                    lc = lc + 1
+            elif(i>='A' and i<='Z'):
+                if(i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+                    uv = uv + 1
+                else:
+                    uc = uc + 1
+            elif(i>='0' and i<='9'):
+                d = d + 1
+            elif(i==' '):
+                s = s + 1
+            else:
+                sp = sp + 1
+            
+    return render(Request,"index.html",{'lv':lv,'lc':lc,'uv':uv,'uc':uc,'d':d,'s':s,'sp':sp,'show':show})
 
 def aboutPage(Request):
     return render(Request, "about.html" )
