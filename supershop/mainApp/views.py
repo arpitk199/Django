5192f409bc36b9b7e7d9a7f9386d33b9869ed15e
arpitk199 <kumararpi199@gmail.com>
2024-03-15 17:05:52 +0530

    some changes

--- supershop/mainApp/views.py
+++ supershop/mainApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.messages import success,error
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout
+from django.contrib.auth import authenticate
 from .models import *
 
 # Create your views here.
@@ -52,6 +56,17 @@ def singleProductPage(Request,id):
     return render(Request,"single-product.html",{'product':product})
 
 def loginPage(Request):
+    if(Request.method=="POST"):
+        username =Request.POST.get("username")
+        password =Request.POST.get("password")
+        user = authenticate(username=username,password=password)
+        if(user is not None):
+            if(user.is_superuser):
+                return HttpResponseRedirect("/admin")
+            else:
+                return HttpResponseRedirect("/profile")
+        else:
+            error(Request,"Invalid username or Password !!!")
     return render(Request,"login.html")
 
 def signupPage(Request):
@@ -59,10 +74,22 @@ def signupPage(Request):
         password = Request.POST.get("password")
         cpassword = Request.POST.get("cpassword")
         if(password==cpassword):
-            name = Request.POST.get("name")
             username = Request.POST.get("username")
             email = Request.POST.get("email")
-            phone = Request.POST.get("phone")
+            name = Request.POST.get("name")
+            try:
+                User.objects.create_user(username=username,email=email,password=password,first_name=name)
+                phone = Request.POST.get("phone")
+
+                b = Buyer()
+                b.name = name
+                b.email = email
+                b.username = username
+                b.phone = phone
+                b.save()
+                return HttpResponseRedirect("/login")
+            except:
+                error(Request,"username Already Exist !!!")
         else:
             error(Request,"Password and Confirm Password Doesn't Matched!!!")
     return render(Request,"signup.html")
